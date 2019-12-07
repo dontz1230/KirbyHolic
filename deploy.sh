71e1ab16998c6f7937bd6faa4b24898c296c906f
Dontz <dontz3210@gmail.com>
2019-12-07 11:44:52 +0800

    fixed swiper

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/dontz1230/kirbyholic.git master:gh-pages
+git push -f https://github.com/dontz1230/KirbyHolic.git master:gh-pages
 
 cd -
